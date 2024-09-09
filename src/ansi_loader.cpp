7b99c5e77e9668238a9eb28e5a3d701460e16f5d
moqui <m0qui@users.noreply.github.com>
2024-09-09 20:34:06 +0200

    ANSI: additional plain-text file extensions
    [moqui] added .nfo file extension to ANSILoader
    [KeyJ] added .diz file extension to ANSILoader
    
    Co-authored-by: Martin Fiedler <keyj@emphy.de>

--- src/ansi_loader.cpp
+++ src/ansi_loader.cpp
@@ -28,6 +28,8 @@ const uint32_t ANSILoader::fileExts[] = {
     // first classic ANSI file extensions
     StringUtil::makeExtCode("asc"),
     StringUtil::makeExtCode("ans"),
+    StringUtil::makeExtCode("nfo"),
+    StringUtil::makeExtCode("diz"),
     // *then* the special binary formats
     StringUtil::makeExtCode("adf"),
     StringUtil::makeExtCode("bin"),
